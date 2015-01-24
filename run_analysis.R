df6a1d2379744fa8a0434f2f21492438fb8430fd
josepnavarro <josep.navarro@alumni.esade.edu>
2015-01-24 19:39:21 +0100

    Getting & cleaning Project
    Full version with 5 steps

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,6 @@
 #
 ##########################################################################################################
 # 1. Merge the training and the test sets to create one data set
-
 # Clean Environment workspace
 rm(list=ls())
 
